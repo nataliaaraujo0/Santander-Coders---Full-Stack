14a3e5679b9a0c3aa9da3797adc0efe68f99a2f7
alucard <devjrteste@gmail.com>
2021-07-16 01:13:40 -0300

    commit - variables
    Tipos de variaveis.

---
+++ lógicadeprogramação/variaveis.js
@@ -0,0 +1,32 @@
+    //vairaveis Primitivas:
+
+//boolean
+var a = true;
+var b = false;
+
+//number
+var c = 10;
+var d = 5;
+
+//string
+var e = "texto qualquer";
+
+//undefined
+var f; 
+var g = undefined;
+
+    //variaveis Complexas:
+
+ //function
+ var h = function () {
+
+ };   
+
+ var i = () => {
+
+ };
+
+ //object
+ var j = {};
+ var l = [];
+ var m = null;
