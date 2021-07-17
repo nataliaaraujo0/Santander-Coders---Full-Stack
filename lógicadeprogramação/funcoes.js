b3c540b328ab51833bf01645556aac67d9828201
alucard <devjrteste@gmail.com>
2021-07-17 15:18:48 -0300

    commit - function

---
+++ lógicadeprogramação/funcoes.js
@@ -0,0 +1,11 @@
+function escreva(){
+    console.log('ola mundo');
+}
+escreva();
+
+//com parametro - texto
+function escrevatexto(texto){
+    console.log(texto);
+}
+
+escrevatexto('Outro Ola mundo');
