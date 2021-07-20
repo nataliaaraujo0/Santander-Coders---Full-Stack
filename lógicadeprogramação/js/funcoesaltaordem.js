bd52c6d3028e1fd0c78b1a2fa5f6a295a7f4acbe
nataliaaraujo0 <81394067+nataliaaraujo0@users.noreply.github.com>
2021-07-20 00:18:33 -0300

    commit - update - file

--- lógicadeprogramação/js/funcoesaltaordem.js
+++ lógicadeprogramação/js/funcoesaltaordem.js
@@ -3,4 +3,6 @@ const subtrair = (a,b) => a-b;
 const aplicaOperacao = (a,b,operacao) => operacao(a,b);
 
 let resultado = aplicaOperacao(4,2,subtrair);
-console.log(resultado)
+console.log(resultado);
+
+
