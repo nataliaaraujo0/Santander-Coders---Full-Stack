743bed438cd81359d4056f5151e1715d8225f422
nataliaaraujo0 <81394067+nataliaaraujo0@users.noreply.github.com>
2021-07-20 18:16:13 -0300

    commit - metodos ->funcao

--- object/objetoPessoa.js
+++ object/objetoPessoa.js
@@ -1,6 +1,19 @@
 const pessoa = {
-    nome:'natalia',
+    nome:'natalia', //atributo
     idade: '25'
 };
 
 console.log(pessoa.idade);
+
+//criando metodo para o objeto -> metodo é como se fosse um atributo que tem uma funcao
+
+const quadrado = {
+    base:'10',
+    altura:'20',
+    calculaArea: function() { 
+        return this.base*this.altura
+    }
+};
+
+//chamar metodo
+console.log(quadrado.calculaArea());
