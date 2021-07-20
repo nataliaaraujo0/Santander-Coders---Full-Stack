53d4993015fe396f93a181b0263109a68f0a5f37
nataliaaraujo0 <81394067+nataliaaraujo0@users.noreply.github.com>
2021-07-20 00:04:31 -0300

    commit - map

---
+++ lógicadeprogramação/funcoesaltaordem.js
@@ -0,0 +1,6 @@
+//Recebendo uma Função como Parâmetro
+const subtrair = (a,b) => a-b;
+const aplicaOperacao = (a,b,operacao) => operacao(a,b);
+
+let resultado = aplicaOperacao(4,2,subtrair);
+console.log(resultado)
---
+++ lógicadeprogramação/map.js
@@ -0,0 +1,8 @@
+//transformar string em number
+const vetor = ["10","20","30"];
+const stringToInt = (x) => parseInt(x);
+const vetor2 = vetor.map(stringToInt);
+console.log(vetor2);
+
+const vetor3 = vetor2.map(x => x*x);
+console.log(vetor3)
